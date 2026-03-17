d7994802351c5ba92e3b174788446bb3fdab277d
rkzarathos <75073398+rkzarathos@users.noreply.github.com>
2026-03-17 11:54:57 -0500

    Remove 'doc18.pdf' from the DOCUMENTS list

--- build_index.py
+++ build_index.py
@@ -41,7 +41,7 @@ DOCUMENTS = [
     "doc1.pdf", "doc2.pdf", "doc3.pdf", "doc4.pdf", "doc5.pdf",
     "doc6.pdf", "doc7.pdf", "doc8.pdf", "doc9.pdf", "doc10.pdf",
     "doc11.pdf", "doc12.pdf", "doc13.pdf", "doc14.pdf", "doc15.pdf",
-    "doc16.pdf", "doc17.pdf", "doc18.pdf", "doc19.pdf", "doc20.pdf",
+    "doc16.pdf", "doc17.pdf", "doc19.pdf", "doc20.pdf",
     "doc21.pdf", "doc22.pdf", "doc23.pdf", "doc24.pdf", "doc25.pdf",
     "doc26.pdf", "doc27.pdf", "doc28.pdf",
 ]
